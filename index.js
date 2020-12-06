e8ec1e22477983b7ee65d1a1b58b441ec0cb14e0
NickCrews <nicholas.b.crews@gmail.com>
2020-12-05 21:50:10 -0700

    First draft of sending comment to a PR
    Uses https://github.com/hmarr/auto-approve-action as an example, found via https://github.com/sdras/awesome-actions. We find the PR number through the github context, and then send a PR comment with a GitHub client authenticaed with secrets.GITHUB_TOKEN.
    
    At this point there is no actual parsing of the git diff to see if gotos are added. Still working on that.
    `$git log -G "goto" --name-only --pretty="" BASEREF..` might be a good start, though it also finds where goto's were removed.

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const wait = require('./wait');
 
-
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    // const token = core.getInput('GITHUB_TOKEN');
-    core.info(github.context)
-    // Available in the build.yml, how to get it here...
-    // const pr = ${{github.event.number}};
+    const token = core.getInput("github-token", { required: true });
 
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const { pull_request: pr } = github.context.payload;
+    if (!pr) {
+      throw new Error("Event payload missing `pull_request`");
+    }
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    const ok = github.getOctokit(token);
+    core.debug(`Sending velociraptors to pull request #${pr.number}`);
+    // await ok.pulls.createReviewComment({
+    await ok.issues.createComment({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      // pull_number: pr.number,
+      issue_number: pr.number,
+      body: 'YOU ADDED A GOTO'
+    });
+    core.debug(`Sent velociraptors to pull request #${pr.number}`);
+    core.setOutput('gotos', 'True');
   } catch (error) {
     core.setFailed(error.message);
   }
