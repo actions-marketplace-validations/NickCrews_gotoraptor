bd31ac738c23398f0f79bd4b58e9a0f7a4e863da
NickCrews <nicholas.b.crews@gmail.com>
2020-12-09 22:07:35 -0700

    Actually include new dist/ files

--- dist/index.js
+++ dist/index.js
@@ -21,30 +21,46 @@ const octokit = github.getOctokit(TOKEN);
 
 const CHECK_NAME = 'Goto Velociraptor Check'
 
-async function getChangedCFiles(ok, owner, repo, pr) {
-  core.debug(`fetching changed files from ${owner}/${repo} PR #${pr}`);
-  const response = await ok.pulls.listFiles({
-    owner: owner,
-    repo: repo,
-    pull_number: pr,
-    page: 0,
-    per_page: 300
-  });
-  const all_filenames = response.data.map(file => file.filename);
-  core.debug(`detected changes in the files ${all_filenames}`)
-  /* regex for c, cc, h, hpp */
+async function getChangedCFiles() {
+  if (isPR()) {
+    // See https://docs.github.com/en/free-pro-team@latest/rest/reference/pulls#list-pull-requests-files
+    const response = await octokit.pulls.listFiles({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      pull_number: github.context.payload.pull_request.nuumber,
+      page: 0,
+      per_page: 300
+    });
+    const files = response.data;
+  } else {
+    // See https://docs.github.com/en/free-pro-team@latest/rest/reference/repos#get-a-commit
+    const response = await octokit.repos.getCommit({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      ref: getHeadSHA()
+    });
+    const files = response.data.files;
+  }
+  core.debug(`All touched files: ${files.map(f => f.filename)}`);
+  // The possible values of GitHub file statuses per
+  // https://github.com/jitterbit/get-changed-files/blob/b17fbb00bdc0c0f63fcf166580804b4d2cdc2a42/src/main.ts#L5
+  // type FileStatus = 'added' | 'modified' | 'removed' | 'renamed'
+  const changedStatuses = ['added', 'modified', 'renamed'];
+  const changedFiles = files.filter(f => changedStatuses.contains(f.status));
+  core.debug(`Files with changes: ${changedFiles.map(f => f.filename)}`);
+  // regex for c, cc, h, hpp
   const pattern = /.*\.[ch](p{2})?$/;
-  const c_filenames = all_filenames.filter(name => name.match(pattern));
-  core.debug(`detected changes in the C/C++ files ${c_filenames}`)
-  return c_filenames;
+  const changedCFiles = changedFiles.filter(f => f.filename.match(pattern));
+  core.debug(`Changed C/C++ files ${changedCFiles.map(f => f.filename)}`)
+  return changedCFiles
 }
 
-function runClangTidy(files) {
+function runClangTidy(filenames) {
     const clang_tidy_path = path.join(clang_tools_bin_dir, 'clang-tidy');
     const { GITHUB_WORKSPACE } = process.env;
     const args = process.argv.slice(2)
         .concat('-checks=-*,cppcoreguidelines-avoid-goto')
-        .concat(files);
+        .concat(filenames);
     const child = proc.spawnSync(clang_tidy_path, args, {
         stdio: 'inherit',
         cwd: GITHUB_WORKSPACE,
@@ -54,13 +70,18 @@ function runClangTidy(files) {
     if (child.status) {
       throw new Error(`clang-tidy failed: ${JSON.stringify(child)}`);
     }
+    core.debug(`clang-tidy stdout: ${child.stdout}`);
     return child.stdout;
+}
+
+function isPR() {
+  return Boolean(github.context.payload.pull_request)
 }
 
 // If we're on a PR, use the sha from the payload to prevent Ghost Check Runs
 // from https://github.com/IgnusG/jest-report-action/blob/de40d98e24f18a77e637762c8d2a1751edfbcc44/tasks/github-api.js#L3
 function getHeadSHA() {
-  if (github.context.payload.pull_request) {
+  if (isPR()) {
     return github.context.payload.pull_request.head.sha;
   }
   return github.context.sha;
@@ -75,6 +96,7 @@ async function sendInitialCheck() {
     status: "in_progress",
     started_at: new Date()
   });
+  core.debug(`Check ID is ${check.data.id}`);
   return check.data.id;
 }
 
@@ -91,21 +113,11 @@ function getVelociraptorMemes() {
   });
 }
 
-// async function getOctokit(token) {
-//   const { Octokit } = require("@octokit/rest");
-//   // const { GITHUB_TOKEN } = process.env;
-//   const octokit = new Octokit({
-//     // auth: GITHUB_TOKEN,
-//     auth: token,
-//     log: {
-//       debug: core.debug,
-//       info: core.info,
-//       warn: core.warn,
-//       error: core.error
-//     }
-//   });
-//   return octokit
-// }
+async function getAddedGotos(){
+  const files = await getChangedCFiles();
+  const gotos = runClangTidy(files.map(f => f.filename));
+  return gotos;
+}
 
 function makeResults(gotos) {
   if (gotos.length == 0) {
@@ -149,22 +161,26 @@ const ERROR_SUMMARY = `Something went wrong internally in the check.
 
 Please file an issue against this [action](https://github.com/NickCrews/gotoraptor/issues/new)`
 
+const ERROR_RESULT = {
+  conclusion: 'failure',
+  output: {
+    title: 'The check errored',
+    summary: ERROR_SUMMARY
+  }
+}
+
 async function run() {
+  core.debug(JSON.stringify(github.context.payload));
+  core.debug(`Running on a ${isPR() ? 'PR' : 'push'} event.`);
   const check_id = await sendInitialCheck();
-  core.debug(`Check ID is ${check_id}`);
   try {
-    const results = makeResults(['goto1'])
+    const gotos = await getAddedGotos();
+    const results = makeResults(gotos)
     await completeCheck(check_id, results);
   } catch (error) {
     core.setFailed(error.message);
     core.error(error.stack);
-    await completeCheck(check_id, {
-      conclusion: 'failure',
-      output: {
-        title: 'The check errored',
-        summary: ERROR_SUMMARY
-      }
-    })
+    await completeCheck(check_id, ERROR_RESULT);
     process.exit(1);
   }
 }
