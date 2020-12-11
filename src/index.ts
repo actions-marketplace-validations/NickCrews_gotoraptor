edf5e06a89fdfc5021657fcda6e6681f35597f53
NickCrews <nicholas.b.crews@gmail.com>
2020-12-11 10:12:50 -0700

    Run formatter for first time

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 // docs.github.com/v3/checks
 
-const path = require('path');
-const proc = require('child_process');
+const path = require("path");
+const proc = require("child_process");
 
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 const TOKEN = core.getInput("github-token", { required: true });
 const octokit = github.getOctokit(TOKEN);
 
-const clang_tools_bin_dir = require('clang-tools-prebuilt');
+const clang_tools_bin_dir = require("clang-tools-prebuilt");
 
-const CHECK_NAME = 'Goto Velociraptor Check'
+const CHECK_NAME = "Goto Velociraptor Check";
 
 async function getChangedCFiles(context) {
   let files;
@@ -22,7 +22,7 @@ async function getChangedCFiles(context) {
       repo: context.repo.repo,
       pull_number: context.payload.pull_request.number,
       page: 0,
-      per_page: 300
+      per_page: 300,
     });
     files = response.data;
   } else {
@@ -30,46 +30,47 @@ async function getChangedCFiles(context) {
     const response = await octokit.repos.getCommit({
       owner: context.repo.owner,
       repo: context.repo.repo,
-      ref: getHeadSHA(context)
+      ref: getHeadSHA(context),
     });
     files = response.data.files;
   }
-  core.debug(`All touched files: ${files.map(f => f.filename)}`);
+  core.debug(`All touched files: ${files.map((f) => f.filename)}`);
   // The possible values of GitHub file statuses per
   // https://github.com/jitterbit/get-changed-files/blob/b17fbb00bdc0c0f63fcf166580804b4d2cdc2a42/src/main.ts#L5
   // type FileStatus = 'added' | 'modified' | 'removed' | 'renamed'
-  const changedStatuses = ['added', 'modified', 'renamed'];
-  const changedFiles = files.filter(f => changedStatuses.includes(f.status));
-  core.debug(`Files with changes: ${changedFiles.map(f => f.filename)}`);
+  const changedStatuses = ["added", "modified", "renamed"];
+  const changedFiles = files.filter((f) => changedStatuses.includes(f.status));
+  core.debug(`Files with changes: ${changedFiles.map((f) => f.filename)}`);
   // regex for c, cc, h, hpp
   const pattern = /.*\.[ch](p{2})?$/;
-  const changedCFiles = changedFiles.filter(f => f.filename.match(pattern));
-  core.debug(`Changed C/C++ files: ${changedCFiles.map(f => f.filename)}`)
-  return changedCFiles
+  const changedCFiles = changedFiles.filter((f) => f.filename.match(pattern));
+  core.debug(`Changed C/C++ files: ${changedCFiles.map((f) => f.filename)}`);
+  return changedCFiles;
 }
 
 function runClangTidy(filenames) {
-    const clang_tidy_path = path.join(clang_tools_bin_dir, 'clang-tidy');
-    const { GITHUB_WORKSPACE } = process.env;
-    const args = process.argv.slice(2)
-        .concat('-checks=-*,cppcoreguidelines-avoid-goto')
-        .concat(filenames);
-    core.debug(`clang-tidy args: ${args}`);
-    const child = proc.spawnSync(clang_tidy_path, args, {
-        stdio: 'inherit',
-        cwd: GITHUB_WORKSPACE,
-        timeout: 30 * 1000
-    });
-    core.debug(`Ran clang-tidy: ${JSON.stringify(child)}`);
-    if (child.status) {
-      throw new Error(`clang-tidy failed: ${JSON.stringify(child)}`);
-    }
-    core.debug(`clang-tidy stdout: ${child.stdout}`);
-    return child.stdout;
+  const clang_tidy_path = path.join(clang_tools_bin_dir, "clang-tidy");
+  const { GITHUB_WORKSPACE } = process.env;
+  const args = process.argv
+    .slice(2)
+    .concat("-checks=-*,cppcoreguidelines-avoid-goto")
+    .concat(filenames);
+  core.debug(`clang-tidy args: ${args}`);
+  const child = proc.spawnSync(clang_tidy_path, args, {
+    stdio: "inherit",
+    cwd: GITHUB_WORKSPACE,
+    timeout: 30 * 1000,
+  });
+  core.debug(`Ran clang-tidy: ${JSON.stringify(child)}`);
+  if (child.status) {
+    throw new Error(`clang-tidy failed: ${JSON.stringify(child)}`);
+  }
+  core.debug(`clang-tidy stdout: ${child.stdout}`);
+  return child.stdout;
 }
 
 function isPR(context) {
-  return Boolean(context.payload.pull_request)
+  return Boolean(context.payload.pull_request);
 }
 
 // If we're on a PR, use the sha from the payload to prevent Ghost Check Runs
@@ -88,56 +89,54 @@ async function sendInitialCheck(context) {
     head_sha: getHeadSHA(context),
     name: CHECK_NAME,
     status: "in_progress",
-    started_at: new Date()
+    started_at: new Date(),
   });
   core.debug(`Check ID is ${check.data.id}`);
   return check.data.id;
 }
 
-const VELOCIRAPTOR_MEME_URLS = [
-  'https://i.imgur.com/wV7InR8.gif'
-]
+const VELOCIRAPTOR_MEME_URLS = ["https://i.imgur.com/wV7InR8.gif"];
 
 function getVelociraptorMemes() {
- return VELOCIRAPTOR_MEME_URLS.map(url => {
-   return {
-     image_url: url,
-     alt: 'velociraptor meme'
-   };
+  return VELOCIRAPTOR_MEME_URLS.map((url) => {
+    return {
+      image_url: url,
+      alt: "velociraptor meme",
+    };
   });
 }
 
-async function getAddedGotos(context){
+async function getAddedGotos(context) {
   const files = await getChangedCFiles(context);
   if (files.length == 0) {
     return [];
   }
-  const gotos = runClangTidy(files.map(f => f.filename));
+  const gotos = runClangTidy(files.map((f) => f.filename));
   return gotos;
 }
 
 function makeResults(gotos) {
   core.debug(`gotos: ${JSON.stringify(gotos)}`);
   if (gotos.length == 0) {
-    core.setOutput('gotos', 'False');
+    core.setOutput("gotos", "False");
     return {
-      conclusion: 'success',
+      conclusion: "success",
       output: {
         title: "No gotos added.",
-        summary: "You got away this time."
-      }
-    }
+        summary: "You got away this time.",
+      },
+    };
   } else {
-    core.setOutput('gotos', 'True');
+    core.setOutput("gotos", "True");
     return {
-      conclusion: 'failure',
+      conclusion: "failure",
       output: {
-        title: 'Velociraptors incoming!',
-        summary: 'gotos were added!',
+        title: "Velociraptors incoming!",
+        summary: "gotos were added!",
         images: getVelociraptorMemes().slice(0, 1),
-        annotations: []
-      }
-    }
+        annotations: [],
+      },
+    };
   }
 }
 
@@ -146,34 +145,34 @@ async function completeCheck(context, check_id, results) {
     owner: context.repo.owner,
     repo: context.repo.repo,
     check_run_id: check_id,
-    status: 'completed',
+    status: "completed",
     conclusion: results.conclusion,
     completed_at: new Date(),
-    output: results.output
-  }
+    output: results.output,
+  };
   core.debug(`Check update request options: ${JSON.stringify(options)}`);
   return await octokit.checks.update(options);
 }
 
 const ERROR_SUMMARY = `Something went wrong internally in the check.
 
-Please file an issue against this [action](https://github.com/NickCrews/gotoraptor/issues/new)`
+Please file an issue against this [action](https://github.com/NickCrews/gotoraptor/issues/new)`;
 
 const ERROR_RESULT = {
-  conclusion: 'failure',
+  conclusion: "failure",
   output: {
-    title: 'The check errored',
-    summary: ERROR_SUMMARY
-  }
-}
+    title: "The check errored",
+    summary: ERROR_SUMMARY,
+  },
+};
 
 async function run(context) {
   core.debug(JSON.stringify(context.payload));
-  core.debug(`Running on a ${isPR(context) ? 'PR' : 'push'} event.`);
+  core.debug(`Running on a ${isPR(context) ? "PR" : "push"} event.`);
   const check_id = await sendInitialCheck(context);
   try {
     const gotos = await getAddedGotos(context);
-    const results = makeResults(gotos)
+    const results = makeResults(gotos);
     await completeCheck(context, check_id, results);
   } catch (error) {
     core.setFailed(error.message);
